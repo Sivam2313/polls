0b1489009f6007b4bd1ff7c0c67bf10cbcb9f5e7
Sivam2313 <sivamdas7@gmail.com>
2023-01-06 17:04:13 +0530

    user auth

--- pollsApp2/urls.py
+++ pollsApp2/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('create/',views.create,name="create"),
     path('vote/<int:pk>',views.Vote.as_view(),name='vote'),
     path('result/<int:pk>',views.result,name = "result"),
-    path('profile/<int:pk>',views.profile,name="profile")
+    path('profile/<int:pk>',views.profile,name="profile"),
+    path('login/',views.login_user,name='login'),
+    path('register/',views.register_user,name='register'),
+    path('logout/',views.logout_user,name='logout'),
+    path('profile/',views.view_profile,name='profile'),
 ]
--- pollsApp2/views.py
+++ pollsApp2/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from .models import Choices, Question
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 class Home (ListView):
     model = Question
@@ -10,9 +13,58 @@ class Home (ListView):
     template_name = "index.html"
 
 def create(request):
+    if request.user is None:
+        return redirect('/login')
     context = {}
+    if request.method == "POST":
+        question = Question.objects.create(question=request.POST['question_name'],created_by=request.user)
+        Choices.objects.create(question=question,choice_text = request.POST['choice3'],count = 0)
+        Choices.objects.create(question=question,choice_text = request.POST['choice1'],count = 0)
+        Choices.objects.create(question=question,choice_text = request.POST['choice2'],count = 0)    
+        return redirect('/polls/home')
     return render(request,'create.html',context)
 
+
+def register_user(request):
+    context = {}
+    if request=="POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = User.objects.create_user(username=username,email=email,password=password)
+        return redirect('/polls/home')
+    return render(request,'register.html')
+
+def login_user(request):
+    context = {
+        "error":"none",
+    }
+    if request=="POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('/polls/home')
+        else:
+            context = {
+                "error":"Wrong Username or Password",
+            }
+            return render(request,"login.html",context)
+    return render(request,'login.html',context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/polls/home')
+
+def view_profile(request):
+    question = Question.objects.all().filter(created_by=request.user)
+    context = {
+        "question_list" : question
+    }
+    return render(request,'profile.html',context)
 
 class Vote  (DetailView):
     model = Question
